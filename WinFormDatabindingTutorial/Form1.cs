fb440b896a974fdc3712fa52b581f37010e567ba
altaygencaslan <wK9R2H6uvFE7s2rMYQg6>
2020-02-01 08:28:03 +0300

    first

---
+++ WinFormDatabindingTutorial/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormDatabindingTutorial
+{
+    public partial class Form1 : Form
+    {
+        public TestClass testClass { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            testClass = new TestClass
+            {
+                Message = "Altay naber?"
+            };
+            
+
+            txt1.DataBindings.Add("Text", testClass, "Message", true, DataSourceUpdateMode.OnPropertyChanged);
+            txt2.DataBindings.Add("Text", testClass, "Message", true, DataSourceUpdateMode.OnPropertyChanged);
+            lbl1.DataBindings.Add("Text", testClass, "Message", true, DataSourceUpdateMode.OnPropertyChanged);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(testClass.Message);
+        }
+    }
+
+    public class TestClass
+    {
+        public string Message { get; set; }
+    }
+}
